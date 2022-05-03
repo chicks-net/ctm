9b73b2abe8064ae401787e90e6451b10cb2ec539
Christopher Hicks <chicks.net@gmail.com>
2022-05-03 11:05:37 -0700

    :hammer: fix non-public struct bug and then whack missing fields in UDP response

--- main.go
+++ main.go
@@ -10,17 +10,24 @@ import (
     "os"
 )
 
+type Time struct {
+    Hour     uint8
+    Minute   uint8
+    Second   uint8
+//    Tenths   uint8
+}
+
 type Response struct {
     DeviceType  uint8
     ClientIP    [4]uint8
     MAC_address [6]uint8
     FirmwareVer [2]uint8
     NTPSyncCnt  uint16
-    DisplayTime [4]uint8
-    DisplayMode uint8
-    Downtimer   uint8
-    unused      [2]uint8
-    WifiSignal  uint8
+    DisplayTime Time
+//    DisplayMode uint8
+//    Downtimer   uint8
+//    Unused      [2]uint8
+//    WifiSignal  uint8
     DeviceName  [16]uint8
 }
 
@@ -52,8 +59,29 @@ func main() {
 	if err != nil {
             panic(err)
         }
-	fmt.Println("code more....")
 
+        fmt.Printf("Type %x\n", struct_resp.DeviceType)
+        fmt.Printf("IP %x\n", struct_resp.ClientIP)
+        fmt.Printf("MAC %x\n", struct_resp.MAC_address)
+        fmt.Printf("Ver %x\n", struct_resp.FirmwareVer)
+        fmt.Printf("Syncs %d\n", struct_resp.NTPSyncCnt)
+        fmt.Printf("Time %d\n", struct_resp.DisplayTime)
+//        fmt.Printf("Mode %x\n", struct_resp.DisplayMode)
+//        if (struct_resp.DisplayMode & 0x40) == 0x40 {
+//	    fmt.Println("\trunning")
+//	} else {
+//	    fmt.Println("\tstopped")
+//	}
+//        if (struct_resp.DisplayMode & 0x80) == 0x80 {
+//	    fmt.Println("\tdisplay M:S:Tenths")
+//	} else {
+//	    fmt.Println("\tdisplay H:M:S")
+//	}
+//        fmt.Printf("Down %x\n", struct_resp.Downtimer)
+//        fmt.Printf("Wifi %x\n", struct_resp.WifiSignal)
+        fmt.Printf("Name %s\n", struct_resp.DeviceName)
+
+	fmt.Println("code more....")
     } else {
         fmt.Printf("Some error %v\n", err)
     }
